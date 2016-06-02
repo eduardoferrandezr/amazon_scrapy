a2dd839a6f6f1d6089a253a354294f01d28f228e
Edu <eduardoferrandezr@gmail.com>
2016-06-02 19:14:21 +0200

    Basic scrapy output

--- amazon_data_science/amazon_data_science/items.py
+++ amazon_data_science/amazon_data_science/items.py
@@ -6,9 +6,10 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy import Field
 
 
 class AmazonDataScienceItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = Field()
+    date = Field()
+    author = Field()
--- amazon_data_science/amazon_data_science/spiders/data_science_books.py
+++ amazon_data_science/amazon_data_science/spiders/data_science_books.py
@@ -1,13 +1,32 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from amazon_data_science.items import AmazonDataScienceItem
 
 class DataScienceBooksSpider(scrapy.Spider):
     name = "data_science_books"
     allowed_domains = ["amazon.com"]
     start_urls = (
-        'http://www.amazon.com/',
+        'http://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=data+science+books',
     )
 
+    def build_item(self, book):
+        title_date_author = book.xpath("descendant::div[@class='a-row a-spacing-small']")
+        title = title_date_author.xpath("a/h2/text()")[0]
+        date = title_date_author.xpath("span[@class='a-size-small a-color-secondary']/text()")[0]
+        author = title_date_author.xpath("div[@class='a-row a-spacing-none']/descendant::*/text()")[1:]
+
+        item = AmazonDataScienceItem()
+        item['title'] = title.extract()
+        item['date'] = date.extract()
+        item['author'] = " ".join(author.extract())
+        return item
+
     def parse(self, response):
-        pass
+        books = response.xpath("//div[@class='s-item-container']")
+        for book in books:
+            yield self.build_item(book)
+
+        next_link = response.xpath("//a[@id='pagnNextLink']/@href").extract()
+        if next_link:
+            url = response.urljoin(next_link[0])
+            yield scrapy.Request(url, self.parse)
